b810387971059c40787787b79aff65a812d4894e
ekhokhlin <evgenii.khokhlin@claroanalytics.com>
2024-10-19 23:22:42 +0300

    week4 (3)

---
+++ src/week4/NumberLineJumps.java
@@ -0,0 +1,34 @@
+package week4;
+
+public class NumberLineJumps {
+
+    public static void main(String[] args) {
+        var input = kangaroo(0, 5, 3, 3);
+        if (input.equals("YES")) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        // Write your code here
+        if (v1 == v2 && x1 != x2) return "NO";
+        int leftPart = x1 - x2;
+        int firstRightPart = v2 - v1;
+        if (leftPart == firstRightPart) return "YES";
+        int firstDelta = Math.abs(Math.abs(leftPart) - Math.abs(firstRightPart));
+        int count = 2;
+        while (true) {
+            int rightPart = count * (v2 - v1);
+            int currentDelta = Math.abs(Math.abs(leftPart) - Math.abs(rightPart));
+            if (firstDelta < currentDelta) {
+                return "NO";
+            }
+            if (leftPart == rightPart) {
+                return "YES";
+            }
+            count++;
+        }
+    }
+}
